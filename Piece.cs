1922293bbe2f6c6edf848c89a1a2796a0ecaa44a
Nickita <nsbeketov@gmail.com>
2025-03-06 19:51:55 +0200

    Git ignore?

---
+++ Piece.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MagicChessBoard
+{
+    public struct Point
+    {
+        public int x;
+        public int y;
+        public Point(int _x, int _y)
+        {
+            x = _x;
+            y = _y;
+        }
+    }
+
+    static public class AllDir
+    {
+        static public Point Up = new Point(0, 1);
+        static public Point UpRight = new Point(1, 1);
+        static public Point Right = new Point(1, 0);
+        static public Point RightDown = new Point(1, -1);
+
+        static public Point Down = new Point(0, -1);
+        static public Point DownLeft = new Point(-1, -1);
+        static public Point Left = new Point(-1, 0);
+        static public Point LeftUp = new Point(-1, 1);
+    }
+
+    class Piece
+    {
+        public enum Type { Pawn, Knight, Rook, Bishop, Queen, King }
+        public enum Color { Black, White}
+        
+
+        public Type type;
+        public Color color;
+        public Point point;
+        public int length;
+
+        public List<Point> dirs;
+                
+        public Piece(Type _type, Color _color, Point _point, int _length)
+        {
+            type = _type;
+            color = _color;
+            point = _point;
+            length = _length;
+
+            SetUpDirs(type);
+        }
+
+        public Piece(Piece other)
+        {
+            type = other.type;
+            color = other.color;
+            point = new Point(other.point.x, other.point.y); // Deep copy
+            length = other.length;
+            dirs = new List<Point>(other.dirs); // Copy list
+        }
+
+        private void SetUpDirs(Type type)
+        {
+            dirs = new List<Point>();
+
+            switch (type)
+            {
+                case Type.Pawn:
+                    {
+                        dirs.Add(AllDir.Up);
+                        dirs.Add(AllDir.Right);
+                        dirs.Add(AllDir.Down);
+                        dirs.Add(AllDir.Left);
+
+                        length = 1;
+                    }
+                    break;
+                case Type.Rook:
+                    {
+                        dirs.Add(AllDir.Up);
+                        dirs.Add(AllDir.Right);
+                        dirs.Add(AllDir.Down);
+                        dirs.Add(AllDir.Left);
+                    }
+                    break;
+                case Type.Bishop:
+                    {
+                        dirs.Add(AllDir.UpRight);
+                        dirs.Add(AllDir.LeftUp);
+                        dirs.Add(AllDir.DownLeft);
+                        dirs.Add(AllDir.RightDown);
+                    }
+                    break;
+                case Type.Queen:
+                    {
+                        dirs.Add(AllDir.Up);
+                        dirs.Add(AllDir.Right);
+                        dirs.Add(AllDir.Down);
+                        dirs.Add(AllDir.Left);
+
+                        dirs.Add(AllDir.UpRight);
+                        dirs.Add(AllDir.LeftUp);
+                        dirs.Add(AllDir.DownLeft);
+                        dirs.Add(AllDir.RightDown);
+                    }
+                    break;
+                case Type.King:
+                    {
+                        dirs.Add(AllDir.Up);
+                        dirs.Add(AllDir.Right);
+                        dirs.Add(AllDir.Down);
+                        dirs.Add(AllDir.Left);
+
+                        dirs.Add(AllDir.UpRight);
+                        dirs.Add(AllDir.LeftUp);
+                        dirs.Add(AllDir.DownLeft);
+                        dirs.Add(AllDir.RightDown);
+
+                        length = 1;
+                    }
+                    break;
+                case Type.Knight:
+                    {
+                        dirs.Add(new Point(2, 1));
+                        dirs.Add(new Point(-2, 1));
+                        dirs.Add(new Point(2, -1));
+                        dirs.Add(new Point(-2, -1));
+
+                        dirs.Add(new Point(1, 2));
+                        dirs.Add(new Point(-1, 2));
+                        dirs.Add(new Point(1, -2));
+                        dirs.Add(new Point(-1, -2));
+
+                        length = 1;
+                    }
+                    break;
+            }
+        }
+
+        public void Info()
+        {
+            Console.WriteLine();
+            Console.WriteLine(type + "[ " + (point.x + 1) + ", " + (char)(point.y + 97)+ "] " + color + " - " + length);
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,520 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace MagicChessBoard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Board board = new Board();
+
+            board.RandomPosition();
+
+            board.ShowBoard(board.charTiles, board.boardPieces);
+
+            board.ShowAllPieceMoves(Piece.Color.White);
+            foreach (var piece in board.piecesB)
+            {
+                //board.ShowPiecePotentMoves(piece);
+                board.ShowPieceLegalMoves(piece);
+            }
+
+            //board.ShowAllPieceMoves(Piece.Color.Black);
+
+        }
+    } 
+    class G {
+        public static string ChangeAnsiColor(string input, int newColorCode)
+        {
+            // Regex pattern to detect ANSI color codes (e.g., "\u001b[36m")
+            string pattern = @"\u001b\[\d+m";
+
+            // Replace the existing color with the new color
+            return Regex.Replace(input, pattern, $"\u001b[{newColorCode}m");
+        }
+    }
+
+    class Board
+    {
+        static public int SizeX = 8;
+        static public int SizeY = 8;
+
+        public List<Piece> piecesW = new List<Piece>();
+        public List<Piece> piecesB = new List<Piece>();
+
+        public Piece[,] boardPieces = new Piece[SizeX, SizeY];
+        public Piece[,] boardPieces_temp = new Piece[SizeX, SizeY];
+
+        public string[,] charTiles = new string[SizeX, SizeY];
+
+        private Piece KingW;
+        private Piece KingB;
+
+        public Board()
+        {
+        }
+
+        public void Test1()
+        {
+            Random random = new Random();
+            int x, y;
+
+            x = 0; y = 0;
+
+            boardPieces[x, y] = new Piece(Piece.Type.King, Piece.Color.White, new Point(x, y), 1);
+            boardPieces_temp[x, y] = new Piece(boardPieces[x, y]);
+
+            piecesW.Add(boardPieces[x, y]);
+            KingW = boardPieces[x, y];
+
+            x = 0; y = 3;
+
+            boardPieces[x, y] = new Piece(Piece.Type.Rook, Piece.Color.Black, new Point(x, y), 4);
+            boardPieces_temp[x, y] = new Piece(boardPieces[x, y]);
+
+            piecesB.Add(boardPieces[x, y]);
+
+            x = 5; y = 0;
+
+            boardPieces[x, y] = new Piece(Piece.Type.Queen, Piece.Color.Black, new Point(x, y), 7);
+            boardPieces_temp[x, y] = new Piece(boardPieces[x, y]);
+
+            piecesB.Add(boardPieces[x, y]);
+
+            x = 7; y = 7;
+
+            boardPieces[x, y] = new Piece(Piece.Type.King, Piece.Color.Black, new Point(x, y), 1);
+            boardPieces_temp[x, y] = new Piece(boardPieces[x, y]);
+
+            piecesB.Add(boardPieces[x, y]);
+            KingB = boardPieces[x, y];
+
+        }
+        public void Test2()
+        {
+            Random random = new Random();
+            int x, y;
+
+            x = 0; y = 0;
+
+            boardPieces[x, y] = new Piece(Piece.Type.King, Piece.Color.White, new Point(x, y), 1);
+            boardPieces_temp[x, y] = new Piece(boardPieces[x, y]);
+
+            piecesW.Add(boardPieces[x, y]);
+            KingW = boardPieces[x, y];
+
+            x = 0; y = 3;
+
+            boardPieces[x, y] = new Piece(Piece.Type.Rook, Piece.Color.Black, new Point(x, y), 4);
+            boardPieces_temp[x, y] = new Piece(boardPieces[x, y]);
+
+            piecesB.Add(boardPieces[x, y]);
+
+            x = 5; y = 5;
+
+            boardPieces[x, y] = new Piece(Piece.Type.Queen, Piece.Color.Black, new Point(x, y), 7);
+            boardPieces_temp[x, y] = new Piece(boardPieces[x, y]);
+
+            piecesB.Add(boardPieces[x, y]);
+
+            x = 7; y = 7;
+
+            boardPieces[x, y] = new Piece(Piece.Type.King, Piece.Color.Black, new Point(x, y), 1);
+            boardPieces_temp[x, y] = new Piece(boardPieces[x, y]);
+
+            piecesB.Add(boardPieces[x, y]);
+            KingB = boardPieces[x, y];
+        }
+        public void Test3()
+        {
+            Random random = new Random();
+            int x, y;
+
+            x = 0; y = 0;
+
+            boardPieces[x, y] = new Piece(Piece.Type.King, Piece.Color.White, new Point(x, y), 1);
+            boardPieces_temp[x, y] = new Piece(boardPieces[x, y]);
+
+            piecesW.Add(boardPieces[x, y]);
+            KingW = boardPieces[x, y];
+
+            x = 1; y = 0;
+
+            boardPieces[x, y] = new Piece(Piece.Type.Rook, Piece.Color.White, new Point(x, y), 4);
+            boardPieces_temp[x, y] = new Piece(boardPieces[x, y]);
+
+            piecesW.Add(boardPieces[x, y]);
+
+            x = 5; y = 5;
+
+            boardPieces[x, y] = new Piece(Piece.Type.Queen, Piece.Color.Black, new Point(x, y), 7);
+            boardPieces_temp[x, y] = new Piece(boardPieces[x, y]);
+
+            piecesB.Add(boardPieces[x, y]);
+
+            x = 7; y = 7;
+
+            boardPieces[x, y] = new Piece(Piece.Type.King, Piece.Color.Black, new Point(x, y), 1);
+            boardPieces_temp[x, y] = new Piece(boardPieces[x, y]);
+
+            piecesB.Add(boardPieces[x, y]);
+            KingB = boardPieces[x, y];
+        }
+        public void Test4()
+        {
+            Random random = new Random();
+            int x, y;
+
+            x = 0; y = 0;
+
+            boardPieces[x, y] = new Piece(Piece.Type.King, Piece.Color.White, new Point(x, y), 1);
+            boardPieces_temp[x, y] = new Piece(boardPieces[x, y]);
+
+            piecesW.Add(boardPieces[x, y]);
+            KingW = boardPieces[x, y];
+
+            x = 1; y = 1;
+
+            boardPieces[x, y] = new Piece(Piece.Type.Bishop, Piece.Color.White, new Point(x, y), 6);
+            boardPieces_temp[x, y] = new Piece(boardPieces[x, y]);
+
+            piecesW.Add(boardPieces[x, y]);
+
+            x = 1; y = 0;
+
+            boardPieces[x, y] = new Piece(Piece.Type.Rook, Piece.Color.White, new Point(x, y), 4);
+            boardPieces_temp[x, y] = new Piece(boardPieces[x, y]);
+
+            piecesW.Add(boardPieces[x, y]);
+
+            x = 5; y = 5;
+
+            boardPieces[x, y] = new Piece(Piece.Type.Queen, Piece.Color.Black, new Point(x, y), 7);
+            boardPieces_temp[x, y] = new Piece(boardPieces[x, y]);
+
+            piecesB.Add(boardPieces[x, y]);
+
+            x = 7; y = 7;
+
+            boardPieces[x, y] = new Piece(Piece.Type.King, Piece.Color.Black, new Point(x, y), 1);
+            boardPieces_temp[x, y] = new Piece(boardPieces[x, y]);
+
+            piecesB.Add(boardPieces[x, y]);
+            KingB = boardPieces[x, y];
+        }
+        public void Test5()
+        {
+            Random random = new Random();
+            int x, y;
+
+            x = 0; y = 0;
+
+            boardPieces[x, y] = new Piece(Piece.Type.King, Piece.Color.White, new Point(x, y), 1);
+            boardPieces_temp[x, y] = new Piece(boardPieces[x, y]);
+
+            piecesW.Add(boardPieces[x, y]);
+            KingW = boardPieces[x, y];
+
+            x = 2; y = 2;
+
+            boardPieces[x, y] = new Piece(Piece.Type.Knight, Piece.Color.White, new Point(x, y), 1);
+            boardPieces_temp[x, y] = new Piece(boardPieces[x, y]);
+
+            piecesW.Add(boardPieces[x, y]);
+
+            x = 0; y = 3;
+
+            boardPieces[x, y] = new Piece(Piece.Type.Queen, Piece.Color.Black, new Point(x, y), 7);
+            boardPieces_temp[x, y] = new Piece(boardPieces[x, y]);
+
+            piecesB.Add(boardPieces[x, y]);
+
+            x = 7; y = 7;
+
+            boardPieces[x, y] = new Piece(Piece.Type.King, Piece.Color.Black, new Point(x, y), 1);
+            boardPieces_temp[x, y] = new Piece(boardPieces[x, y]);
+
+            piecesB.Add(boardPieces[x, y]);
+            KingB = boardPieces[x, y];
+        }
+
+        public void RandomPosition()
+        {
+            Random random = new Random();
+
+            int count = random.Next(0, 16);
+
+            int x;
+            int y;
+
+            for (int i = 0; i < count; i++)
+            {
+                int length = random.Next(1, 8);
+
+                x = random.Next(0, SizeX);
+                y = random.Next(0, SizeY);
+
+                Piece.Color color = (Piece.Color)(random.Next(0, 100) % 2);
+                Piece.Type type = (Piece.Type)(random.Next(0, 100) % 5);
+                
+                boardPieces[x, y] = new Piece(type, color, new Point(x, y), length);
+                boardPieces_temp[x, y] = new Piece(type, color, new Point(x, y), length);
+
+                if (color == Piece.Color.White) piecesW.Add(boardPieces[x, y]);
+                else piecesB.Add(boardPieces[x, y]);
+            }
+
+            x = random.Next(0, SizeX);
+            y = random.Next(0, SizeY);
+
+            boardPieces[x, y] = new Piece(Piece.Type.King, Piece.Color.White, new Point(x, y), 1);
+            boardPieces_temp[x, y] = new Piece(boardPieces[x, y]);
+
+            piecesW.Add(boardPieces[x, y]);
+            KingW = boardPieces[x, y];
+
+            x = random.Next(0, SizeX);
+            y = random.Next(0, SizeY);
+
+            boardPieces[x, y] = new Piece(Piece.Type.King, Piece.Color.Black, new Point(x, y), 1);
+            boardPieces_temp[x, y] = new Piece(boardPieces[x, y]);
+
+            piecesB.Add(boardPieces[x, y]);
+            KingB = boardPieces[x, y];
+
+            SetUpChars(boardPieces);
+        }
+
+        private void SetUpChars(Piece[,] board)
+        {
+            for (int x = 0; x < SizeX; x++)
+            {
+                for (int y = 0; y < SizeY; y++)
+                {
+                    if (board[x, y] == null) charTiles[x, y] = "\u001b[0m.\u001b[0m";
+                    else
+                    {
+                        string color, net = "\u001b[0m";
+
+                        if (board[x, y].color == Piece.Color.White) color = "\u001b[36m";
+                        else color = "\u001b[31m";
+
+                        switch (board[x, y].type)
+                        {
+                            case Piece.Type.Pawn:
+                                {
+                                    charTiles[x, y] = color + "P" + net;
+                                }
+                                break;
+                            case Piece.Type.Rook:
+                                {
+                                    charTiles[x, y] = color + "R" + net;
+                                }
+                                break;
+                            case Piece.Type.Bishop:
+                                {
+                                    charTiles[x, y] = color + "B" + net;
+                                }
+                                break;
+                            case Piece.Type.Queen:
+                                {
+                                    charTiles[x, y] = color + "Q" + net;
+                                }
+                                break;
+                            case Piece.Type.King:
+                                {
+                                    charTiles[x, y] = color + "K" + net;
+                                }
+                                break;
+                            case Piece.Type.Knight:
+                                {
+                                    charTiles[x, y] = color + "N" + net;
+                                }
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+
+        public void ShowBoard(string[,] tiles, Piece[,] board)
+        {
+            SetUpChars(board);
+
+            Console.WriteLine("+ a b c d e f g h");
+
+            for (int x = 0; x < SizeX; x++)
+            {
+                Console.Write(x + 1);
+
+                for (int y = 0; y < SizeY; y++)
+                {
+                    Console.Write(" ");
+                    Console.Write(tiles[x, y]);
+                }
+
+                Console.Write("\n");
+            }
+        }
+
+        private void Board_TempMove(Piece piece, Point move)
+        {
+            boardPieces_temp[piece.point.x, piece.point.y] = null;
+            boardPieces_temp[move.x, move.y] = piece;
+            piece.point = move;
+        }
+
+        private void Board_ReturnMove(Piece piece, Point move)
+        {
+            Piece temp_piece = boardPieces[piece.point.x, piece.point.y];
+            if (temp_piece != null) boardPieces_temp[piece.point.x, piece.point.y] = new Piece(temp_piece);
+            else boardPieces_temp[piece.point.x, piece.point.y] = null;
+
+            temp_piece = boardPieces[move.x, move.y];
+            if (temp_piece != null) boardPieces_temp[move.x, move.y] = new Piece(temp_piece);
+            else boardPieces_temp[move.x, move.y] = null;
+
+            piece.point = move;
+        }
+
+        private bool KingSave(Piece.Color color)
+        {
+            for (int x = 0; x < SizeX; x++)
+            {
+                for (int y = 0; y < SizeY; y++)
+                {
+                    if (boardPieces_temp[x, y] == null) continue;
+                    if (boardPieces_temp[x, y].color == color) continue;
+
+                    if (color == Piece.Color.Black) if (PotentMove(boardPieces_temp[x, y], boardPieces_temp).Contains(KingB.point)) return false;
+                    if (color == Piece.Color.White) if (PotentMove(boardPieces_temp[x, y], boardPieces_temp).Contains(KingW.point)) return false;
+                }
+            }
+
+            return true;
+        }
+
+        private List<Point> PotentMove(Piece piece, Piece[,] board)
+        {
+            List<Point> potent_moves = new List<Point>();
+
+            Point ocupPos = piece.point;
+            Point nextPos = piece.point;
+
+            foreach (var dir in piece.dirs)
+            {
+                for (int i = 1; i <= piece.length; i++)
+                {
+                    nextPos.x = i * dir.x + ocupPos.x;
+                    nextPos.y = i * dir.y + ocupPos.y;
+
+                    // from there 
+                    if (nextPos.x >= SizeX || nextPos.x < 0 || nextPos.y >= SizeY || nextPos.y < 0) break; 
+                    else if (board[nextPos.x, nextPos.y] != null && board[nextPos.x, nextPos.y].color == piece.color) break; 
+                    else if (board[nextPos.x, nextPos.y] != null && board[nextPos.x, nextPos.y].color != piece.color) { potent_moves.Add(nextPos); break; } 
+                    else potent_moves.Add(nextPos);
+
+                    // to there potetial moves
+                }
+            }
+
+            return potent_moves;
+        }
+
+        private List<Point> LegalMove(Piece piece, List<Point> potent_moves)
+        {
+            List<Point> legal_moves = new List<Point>();
+            Point startPos = piece.point;
+
+            foreach (var move in potent_moves)
+            {
+                // take move
+                Board_TempMove(piece, move);
+                
+                //ShowBoard(charTiles, boardPieces_temp);
+
+                // check king save And add move
+                if (KingSave(piece.color))legal_moves.Add(move); 
+
+                // return state
+                Board_ReturnMove(piece, startPos);
+                
+                SetUpChars(boardPieces);
+            }
+
+            
+
+            return legal_moves;
+        }
+
+        public void ShowPiecePotentMoves(Piece piece)
+        {
+            piece.Info();
+
+            Point ocupPos = piece.point;
+
+            List<Point> potent_moves = PotentMove(piece, boardPieces);
+
+            string[,] tempCharTiles = charTiles.Clone() as string[,];
+            tempCharTiles[ocupPos.x, ocupPos.y] = G.ChangeAnsiColor(tempCharTiles[ocupPos.x, ocupPos.y], 33) + "\u001b[0m";
+
+            foreach (var pos in potent_moves)
+            {
+                tempCharTiles[pos.x, pos.y] = G.ChangeAnsiColor(tempCharTiles[pos.x, pos.y], 33) + "\u001b[0m";
+            }
+
+            ShowBoard(tempCharTiles, boardPieces);
+        }
+
+        public void ShowPieceLegalMoves(Piece piece)
+        {
+            piece.Info();
+
+            Point ocupPos = piece.point;
+
+            List<Point> potent_moves = PotentMove(piece, boardPieces);
+            List<Point> legal_moves = LegalMove(piece, potent_moves);
+
+            string[,] tempCharTiles = charTiles.Clone() as string[,];
+            tempCharTiles[ocupPos.x, ocupPos.y] = G.ChangeAnsiColor(tempCharTiles[ocupPos.x, ocupPos.y], 32) + "\u001b[0m";
+
+            foreach (var pos in legal_moves)
+            {
+                tempCharTiles[pos.x, pos.y] = G.ChangeAnsiColor(tempCharTiles[pos.x, pos.y], 32) + "\u001b[0m";
+            }
+
+            ShowBoard(tempCharTiles, boardPieces);
+        }
+
+        public void ShowAllPieceMoves(Piece.Color color)
+        {
+            string[,] tempCharTiles = charTiles.Clone() as string[,];
+
+            List<Piece> pieces = new List<Piece>();
+            if (color == Piece.Color.White) pieces = piecesW;
+            else if (color == Piece.Color.Black) pieces = piecesB;
+
+            foreach (var piece in pieces)
+            {
+                Point ocupPos = piece.point;
+
+                List<Point> potent_moves = PotentMove(piece, boardPieces);
+                List<Point> legal_moves = LegalMove(piece, potent_moves);
+
+                
+                tempCharTiles[ocupPos.x, ocupPos.y] = G.ChangeAnsiColor(tempCharTiles[ocupPos.x, ocupPos.y], 33) + "\u001b[0m";
+
+                foreach (var pos in legal_moves)
+                {
+                    tempCharTiles[pos.x, pos.y] = G.ChangeAnsiColor(tempCharTiles[pos.x, pos.y], 33) + "\u001b[0m";
+                }
+            }
+
+            ShowBoard(tempCharTiles, boardPieces);
+        }
+
+
+    }
+}
